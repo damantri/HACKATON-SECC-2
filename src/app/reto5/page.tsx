784f5ee642b0aa4b5cc8dd2838ce1a483a6fca3c
Miguel Ariza <m.arizaj@uniandes.edu.co>
2025-10-08 09:12:29 -0500

    reto5bug

--- src/app/reto5/page.tsx
+++ src/app/reto5/page.tsx
@@ -5,12 +5,14 @@ export default function Reto5() {
     <div className="min-h-screen bg-[#0f172a] flex flex-col items-center justify-center gap-6 text-white">
       <nav className="w-full max-w-5xl bg-[#1e293b] py-3 px-8 flex justify-between items-center shadow-lg">
         <div className="text-2xl font-bold">Navbar</div>
+
         <ul className="flex space-x-6 text-gray-300">
           <li className="hover:text-white cursor-pointer">Home</li>
           <li className="hover:text-white cursor-pointer">Features</li>
           <li className="hover:text-white cursor-pointer">Pricing</li>
           <li className="hover:text-white cursor-pointer">About</li>
         </ul>
+
         <div className="flex items-center space-x-2">
           <input
             type="text"
@@ -22,16 +24,16 @@ export default function Reto5() {
           </button>
         </div>
       </nav>
-
-      {/* Navbar invertida con funcion de espejo */}
-      <nav className="w-full max-w-5xl bg-[#1e293b] py-3 px-8 flex justify-between items-center shadow-lg rotate-180">
+      <nav className="w-full max-w-5xl bg-[#1e293b] py-3 px-8 flex justify-between items-center shadow-lg scale-x-[-1]">
         <div className="text-2xl font-bold">Navbar</div>
+
         <ul className="flex space-x-6 text-gray-300">
           <li>Home</li>
           <li>Features</li>
           <li>Pricing</li>
           <li>About</li>
         </ul>
+
         <div className="flex items-center space-x-2">
           <input
             type="text"
